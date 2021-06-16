554fcecdca5c8022e222a0a007ee97a6236edc12
Vincent Corbee <vincent@Vincents-MacBook-Pro-3.local>
2021-06-16 16:52:21 +0200

    chore(refactoring): refactored getArgs

--- lib/get-args/get-args.ts
+++ lib/get-args/get-args.ts
@@ -3,6 +3,7 @@ import { regExp } from '../constants'
 import { ParsedArgs } from '../types'
 
 const strip = (key: string, reg: RegExp) =>  key.replace(reg, '')
+const test = (key: string, reg: RegExp) => reg.test(key)
 
 export const getArgs = (delim = '-', args: string[] | string = process.argv.slice(2)): ParsedArgs => {
   args = Array.isArray(args) ? args : args.split(' ')
@@ -10,30 +11,30 @@ export const getArgs = (delim = '-', args: string[] | string = process.argv.slic
   let ch = 1
 
   const parsedArgs: ParsedArgs = {}
-  const regKey = new RegExp(`^[^${delim}]`)
-  const regDelim = new RegExp(`^${delim}`)
+  const regValue = new RegExp(`^[^${delim}]`)
+  const regKey = new RegExp(`^${delim}`)
 
   for (let i = 0, l = args.length; i < l; i += ch) {
-    if (!regDelim.test(args[i]) || regDelim.test(args[i + 1])) {
-      parsedArgs[strip(args[i], regDelim)] = true
+    if (!test(args[i], regKey) || test(args[i + 1], regKey)) {
+      parsedArgs[strip(args[i], regKey)] = true
 
       ch = 1
     } else {
       ch = 2
 
       const [key, val] = args.slice(i, i + ch)
-      const argName = strip(key, regDelim)
+      const argName = strip(key, regKey)
 
-      if (regKey.test(args[i + ch]) && i + ch < l) {
+      if (test(args[i + ch], regValue) && i + ch < l) {
         const list = [getValue(strip(val, regExp.COMMA))]
 
-        while (regKey.test(args[i + ch]) && i + ch < l) {
+        while (test(args[i + ch], regValue) && i + ch < l) {
           list.push(getValue(strip(args[i + ch],regExp.COMMA)))
           ch += 1
         }
 
         parsedArgs[argName] = list
-      } else if (val !== undefined && !regDelim.test(val)) {
+      } else if (val !== undefined && !test(val, regKey)) {
         parsedArgs[argName] = getValue(val)
       } else {
         parsedArgs[argName] = true
